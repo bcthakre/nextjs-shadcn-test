bd7a69653c789a96033c20a4d26f629b499d2c67
bcthakre <bcthakregcp@gmail.com>
2024-01-13 22:35:35 -0600

    Refactor code to improve readability and add recipe cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle  } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface Recipe {
   title: string;
@@ -15,17 +22,29 @@ async function getRecipes(): Promise<Recipe[]> {
 }
 
 export default async function Home() {
-
   const recipes = await getRecipes();
 
-  
   return (
     <>
-    <div className="grid grid-cols-3 gap-8">
-      {recipes.map(recipe => (
-
-      ))}
-    </div>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins. to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
-  ) ;
+  );
 }
