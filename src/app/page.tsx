e4b01aad5744a28e33b5c94d17b1b7978519e0fa
bcthakre <bcthakregcp@gmail.com>
2024-01-13 20:23:22 -0600

    Add Card component and fetch recipes from server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle  } from "@/components/ui/card";
 
-export default function Home() {
-  return <main>home</main>;
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+  return result.json();
+}
+
+export default async function Home() {
+
+  const recipes = await getRecipes();
+
+  
+  return (
+    <>
+    <div className="grid grid-cols-3 gap-8">
+      {recipes.map(recipe => (
+
+      ))}
+    </div>
+    </>
+  ) ;
 }
