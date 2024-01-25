8eeef1e6fd5ec36f9559d3f7ff62dcf0be1714ce
bcthakre <bcthakregcp@gmail.com>
2024-01-24 20:07:06 -0600

    made changes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 function Logo() {
   return (
     <>
-      <Link href={"/"}>
-        <div>
+      <Link prefetch={false} href={"/"} className="overflow-hidden">
+        <div className="flex items-center w-72 h-14">
           <AspectRatio
             ratio={16 / 9}
             className="flex items-center justify-center"
