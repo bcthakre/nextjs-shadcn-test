663c6241d43900990120246fb468fd1aa41e9234
bcthakre <bcthakregcp@gmail.com>
2024-01-27 17:39:40 -0600

    Update dependencies and add framer-motion library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,50 @@ import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { TypewriterEffect } from "@/components/ui/typewrite-effect";
 
 export default function Home() {
   const { data, status } = useSession();
   const router = useRouter();
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Aceternity.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
 
 
   console.log(data);
   return (
     <>
       <main>Hello World</main>
+      <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffect words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Join now
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Signup
+        </button>
+      </div>
+    </div>
+      
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,27 @@ import { DarkModeToggle } from "./DarkModeToggle";
 import UserButton from "./UserButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth";
+import { MessagesSquareIcon } from "lucide-react";
+import Link from "next/link";
 
 async function Header() {
+  const session = await getServerSession(authOptions);
 
-  const session = await getServerSession(authOptions)
-
-  console.log(session)
   return (
     <header className="sticky z-50 top-0 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          { session && (
+          {session ? (
             <>
-            
+              <Link href={"/chat"} prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
             </>
+          ) : (
+            <Link href={"/pricing"} prefetch={false}>
+              Pricing
+            </Link>
           )}
           <DarkModeToggle />
           <UserButton />
