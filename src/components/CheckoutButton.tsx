1171245cb466792d9634ef8ae7e9626ef09beec1
bcthakre <bcthakregcp@gmail.com>
2024-02-01 00:28:37 -0600

    Add Stripe secret key and implement checkout session creation

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -1,13 +1,30 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { useSession } from "next-auth/react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 
 function CheckoutButton() {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
   const createCheckoutSession = async () => {
-    if (!session) return;
+    if (!session?.user.id) return;
+
+    // push a document to firebase db
+
+    setLoading(true);
+
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_sessions"),
+      {
+        price: "price_1OetglDblx6VCaujtkWDv0Ln",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
   };
 
   return (
--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -21,7 +21,7 @@ const tiers = [
   },
   {
     name: "Pro",
-    id: "si_OnlcsLNQYbMVzV",
+    id: "pro",
     href: "#",
     priceMonthly: "£5.99",
     description: "Unlock the Full Potential with Pro!",
