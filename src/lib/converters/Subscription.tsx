c36399fc4633936182401f3227e3db0e2fbffcae
bcthakre <bcthakregcp@gmail.com>
2024-02-02 10:59:08 -0600

    Add Subscription converter and Subscription type

---
+++ src/lib/converters/Subscription.tsx
@@ -0,0 +1,52 @@
+import { Subscription } from "../../../types/Subscription";
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+} from "firebase/firestore";
+
+import { db } from "../../../firebase";
+import { Subscript } from "lucide-react";
+
+const subscriptionConverter: FirestoreDataConverter<Subscription> = {
+    toFirestore: function (subscription : Subscription): DocumentData {
+        return {
+            ...subscription
+        }
+    },
+    fromFirestore: function (
+        snapShot : QueryDocumentSnapshot,
+        options : SnapshotOptions
+    ) : Subscription {
+        const data = snapShot.data(options);
+        
+        
+        return sub : Subscription = {
+            id: snapshot.id,
+            cancel_at_period_end: data.cancel_at_period_end,
+            created: data.created,
+            current_period_start: data.current_period_start,
+            items: data.items,
+            latest_invoice: data.latest_invoice,
+            metadata: data.metadata,
+            payment_method: data.payment_method,
+            price: data.price,
+            prices: data.prices,
+            product: data.product,
+            quantity: data.quantity,
+            status: data.status,
+            stripeLink: data.stripeLink,
+            cancel_at: data.cancel_at,
+            canceled_at: data.canceled_at,
+            current_period_end: data.current_period_end,
+            ended_at: data.ended_at,
+            trial_start: data.trial_start,
+            trial_end: data.trial_end,
+            role: data.role,
+        }
+    }
+
+
+}
