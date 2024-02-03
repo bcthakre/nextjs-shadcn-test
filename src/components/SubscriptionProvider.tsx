7cebd2cc4a4728bcfebd62551235f6245e166182
bcthakre <bcthakregcp@gmail.com>
2024-02-02 21:10:49 -0600

    Refactor SubscriptionProvider and Subscription converter

--- src/components/SubscriptionProvider.tsx
+++ src/components/SubscriptionProvider.tsx
@@ -1,12 +1,27 @@
 "use client";
 
+import { subscriptionRef } from "@/lib/converters/Subscription";
+import { onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 function SubscriptionProvider() {
   const { data: session } = useSession();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!session) return;
+
+    return onSnapshot(subscriptionRef(session?.user.id), (snapshot) => {
+      if (snapshot.empty) {
+        console.log("No matching documents.");
+        // set no subscription
+        return;
+      } else {
+        console.log("User has subscription");
+        // set subscription
+      }
+    });
+  }, [session]);
   return <div>SubscriptionProvider</div>;
 }
 
